3965065b290999bb131be421a7e65e63cf15e46d
Noel9356 <noel.poetica.caelus@gmail.com>
2026-03-12 01:41:18 +0700

    .

--- obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
+++ obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49715e7fac8acc2912fb06742d24c450842977a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6a318f8dee73f0cd63f57580776b077a8d09b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
