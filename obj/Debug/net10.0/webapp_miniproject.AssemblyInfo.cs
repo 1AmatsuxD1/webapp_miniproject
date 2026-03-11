f48893c06b961a3277e34674e8940f4cf67f1ece
R'Heng!!! <chawanwit9899@gmail.com>
2026-03-12 01:46:01 +0700

    chache

--- obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
+++ obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+376ebfab3d055b33a450f9462aaf9fc8341e4446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8754056345832a3a21eace4fe1cde4797e0485")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
