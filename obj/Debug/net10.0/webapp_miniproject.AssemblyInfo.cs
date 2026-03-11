5f3b7d15464eff90e61d3ec9e8e2c56b91bd1ad9
Noel9356 <noel.poetica.caelus@gmail.com>
2026-03-11 23:51:27 +0700

    Fix player counting error. Forgot to stage changes on the previous commit.

--- obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
+++ obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d463dbb320530eca237d78199fa8899e27cadb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5845698513e56a6641211ac7690b5b57d70a22f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
