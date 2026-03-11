b49715e7fac8acc2912fb06742d24c450842977a
Noel9356 <noel.poetica.caelus@gmail.com>
2026-03-12 01:39:06 +0700

    Remove review.

--- obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
+++ obj/Debug/net10.0/webapp_miniproject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7e99c508abd8974f737d47896017a452273b957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7690a39cf453a488f3d978596106949db8dc8f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapp_miniproject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
